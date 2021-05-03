cbe05022b1daa4726474c3c6da4dd074f9006100
Vincent He <vincent0710@Outlook.com>
2021-05-03 16:20:32 -0400

    Created two solver class: Solver and LinearEquation

---
+++ solver.py
@@ -0,0 +1,48 @@
+import os
+import pickle
+
+
+class Solver():
+    def __init__(self, contours):
+        self.contours = contours
+
+    def solve(self, max_equations=100):
+        equations_per_contour = int(max_equations/len(self.contours))
+        for contour in self.contours:
+            split_ratio = int(len(contour) / equations_per_contour)
+
+            for i in range(equations_per_contour):
+                start_point = contour[i * split_ratio]
+                x1, y1 = start_point[0][0], start_point[0][1]
+                end_point = contour[(i+1) * split_ratio]
+                x2, y2 = end_point[0][0], end_point[0][1]
+
+                if x1-x2 != 0:
+                    k = (y1-y2)/(x1-x2)# The slope of the line
+                    b = y1 - (k*x1) # The line offset
+
+                    equation = LinearEquations(k, b, x1, x2)
+                    equation.save()
+
+class LinearEquations():
+    save_dir = os.path.join(os.curdir, "Equations")
+
+    def __init__(self, k, b, x1, x2, save=False):
+        self.k = k
+        self.b = b
+        self.x1 = x1
+        self.x2 = x2
+
+        if save:
+            self.save()
+
+    def save(self):
+        self.mkdir(self.save_dir)
+        with open(os.path.join(self.save_dir, f"f(x) = {self.k}x + {self.b}.pickle"), 'wb') as f:
+            pickle.dump(self, f)
+
+    def mkdir(self, path):
+        try:
+            os.mkdir(path)
+        except Exception as e:
+            pass
